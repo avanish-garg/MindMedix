6854e8d7be4a85fca69afd541f5cad65b06ae036
Avanish Garg <avanishgarg6734@gmail.com>
2024-08-22 19:58:34 +0530

    commented env and used directly now mongo is connecting!

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,28 @@
-// backend/app.js or backend/server.js
+// backend/server.js
+
+// require('dotenv').config();
+
+const mongoURI = 'mongodb+srv://avanish:jaijinendra@pandavastrial.n7bpfrh.mongodb.net/?retryWrites=true&w=majority&appName=PANDAVASTRIAL';
 const express = require('express');
 const mongoose = require('mongoose');
-const progressRoutes = require('./routes/progress');
-const dotenv = require('dotenv');
-const moodRoutes = require('./routes/mood');
-require('dotenv').config();
 
-// Load environment variables from .env file
-dotenv.config();
+const progressRoutes = require('./routes/progress');
+const moodRoutes = require('./routes/mood');
+
 
 const app = express();
 app.use(express.json());
 
-// Connect to MongoDB Atlas using Mongoose
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB connected...'))
 .catch(err => console.error('MongoDB connection error:', err));
-
 // Use Routes
 app.use('/api/progress', progressRoutes);
 app.use('/api/mood', moodRoutes);
 
+// Set the server to listen on the specified port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
